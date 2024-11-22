d82e63294b64bdaeb6fc9a312443ffd6b3b56c63
rikta322 <rikta.jannatulferdous@gmail.com>
2024-11-22 13:31:59 +0600

    homepage,header,footer and base html and views,urls

--- srkFashionHouse/srkFashionHouse/urls.py
+++ srkFashionHouse/srkFashionHouse/urls.py
@@ -1,7 +1,7 @@
 """
 URL configuration for srkFashionHouse project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.contrib.auth.views import LoginView
+from .import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('base/',views.base,name='base'),
+    path('',views.home,name='home'),
 ]
